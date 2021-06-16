22a4bb3cbe65bda6f65f6ed1f2e68e9834cf55bc
Yoonus <yoonustehrani28@gmail.com>
2021-06-16 08:37:55 +0430

    mix is working correctly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Daya Artz</title>
 </head>
 <body>
-    <h1>Welcome to DayaArtz</h1>
+    <h1 class="p-3">Welcome to DayaArtz</h1>
 </body>
 </html>
