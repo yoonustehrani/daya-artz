7936d590899958ba2439493ec740d22507db561e
Yoonus <yoonustehrani28@gmail.com>
2021-06-17 19:04:12 +0430

    submenu is finalized

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,28 @@
                 <img src="{{ asset('images/daya-white-logo.png') }}">
             </div>
             <ul class="main-menu p-0 m-0">
-                <li><a href="#">خانه</a></li>
-                <li><a href="#">طراحی گرافیک</a></li>
+                <li>
+                    <a href="#">خانه</a>
+                </li>
+                <li>
+                    <a href="#">طراحی گرافیک</a>
+                    <ul class="submenu">
+                        <li><a href="#">زیرمجموعه 1</a></li>
+                        <li><a href="#">زیرمجموعه 2</a></li>
+                        <li><a href="#">زیرمجموعه 3</a></li>
+                        <li><a href="#">زیرمجموعه 4</a></li>
+                        <li><a href="#">زیرمجموعه 5</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">زیرمجموعه دار</a>
+                    <ul class="submenu">
+                        <li><a href="#">زیرمجموعه 1</a></li>
+                        <li><a href="#1">زیرمجموعه طولانی</a></li>
+                        <li><a href="#2">زیرمجموعه یخورده طولانی تر</a></li>
+                        <li><a href="#3">زیرمجموعه خیلــــــــی طولانی</a></li>
+                    </ul>
+                </li>
                 <li><a href="#">مقالات</a></li>
                 <li><a href="#">درباره ما</a></li>
                 <li><a href="#">تماس با ما</a></li>
@@ -26,6 +46,9 @@
                 <button class="btn-phone mr-3"><i class="fas fa-phone"></i></button>
             </div>
         </nav>
+        <div>
+            
+        </div>
     </header>
 </body>
 </html>
