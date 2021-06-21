b348ce00aabbdc6462194f3706f4e1ebbedadb5e
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-06-21 13:05:14 +0430

    fast order section designed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -354,40 +354,41 @@
     </div>
     <!-- end Portfolio -->
     <!-- fast order -->
-    <div class="section fast-order-container w-100">
-        <div class="title-section w-100">
+    <div class="section fast-order-container w-100 mb-5">
+        <div class="title-section w-100 mb-5">
             <div class="title-container">
                 <h2 class="title-text">سفارش فوری</h2>
                 <span class="title-underline"></span>
             </div>
-            <div class="fast-order-section text-center w-100 offset-md-1">
-                <div class="float-left fast-order-form col-12 col-md-5">
-                    <form action="#">
-                        <div class="form-group"><input type="text" name="fast-order-name" class="form-control" placeholder="نام و نام خانوادگی"></div>
-                        <div class="form-group"><input type="text" name="fast-order-number" class="form-control" placeholder="شماره تلفن"></div>
-                        <div class="order-types w-100">
-                            <div class="checkbox col-5 col-md-2"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/brand.svg') }}"></span><span class="checkbox-label">لوگو</span></span></label></div>
-                            <div class="checkbox col-5 col-md-2"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/big-brochure.svg') }}"></span><span class="checkbox-label">کاتالوگ</span></span></label></div>
-                            <div class="checkbox col-5 col-md-2"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/credit-card.svg') }}"></span><span class="checkbox-label">کارت ویزیت</span></span></label></div>
-                            <div class="checkbox col-5 col-md-2"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/paper.svg') }}"></span><span class="checkbox-label">سربرگ</span></span></label></div>
-                            <div class="checkbox col-5 col-md-2"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/office-tools.svg') }}"></span><span class="checkbox-label">ست اداری</span></span></label></div>
-                        </div>
-                        <div class="form-group"><textarea name="fast-order-description" cols="30" rows="10" class="form-control" placeholder="توضیحات"></textarea></div>
-                        <button type="submit" class="btn btn-primary w-100">ثبت سفارش</button>
-                    </form>
-                </div>
-                <div class="float-left col-12 col-md-5 fast-order-guide">
-                    <h3>فرم سفارش فوری</h3>
-                    <p class="font-20">با پر کردن این فرم و ثبت نوع طراحی کورد نیاز خود منتظر تماس کارشناسان باشید.</p>
-                    <br><br>
-                    <h5 class="font-20">راهنمای ثبت فرم:</h5>
-                    <ul class="font-20">
-                        <li>وارد کردن مشخصات شخصی</li>
-                        <li>وارد کردن شماره تماس در دسترس</li>
-                        <li>انتخاب درسته مورد نیاز برای طراحی</li>
-                    </ul>
-                    <br><br>
-                    <p class="font-18 text-secondary">توجه داشته باشید حداقل زمان تماس کارشناسان با شما بعد از ثبت سفارش بین ۱۲ الی ۲۴ ساعت میباشد.</p>
+        </div>
+        <div class="fast-order-section text-center w-100 offset-md-1">
+            <div class="float-left fast-order-form col-12 col-md-6">
+                <form action="#">
+                    <div class="form-group"><input type="text" name="fast-order-name" class="form-control" placeholder="نام و نام خانوادگی"></div>
+                    <div class="form-group"><input type="text" name="fast-order-number" class="form-control" placeholder="شماره تلفن"></div>
+                    <div class="order-types w-100">
+                        <div class="checkbox col-5 col-md-4 col-lg-2 p-0"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/brand.svg') }}"></span><span class="checkbox-label">لوگو</span></span></label></div>
+                        <div class="checkbox col-5 col-md-4 col-lg-2 p-0"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/big-brochure.svg') }}"></span><span class="checkbox-label">کاتالوگ</span></span></label></div>
+                        <div class="checkbox col-5 col-md-4 col-lg-2 p-0"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/credit-card.svg') }}"></span><span class="checkbox-label">کارت ویزیت</span></span></label></div>
+                        <div class="checkbox col-5 col-md-4 col-lg-2 p-0"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/paper.svg') }}"></span><span class="checkbox-label">سربرگ</span></span></label></div>
+                        <div class="checkbox col-5 col-md-4 col-lg-2 p-0"><label class="checkbox-wrapper"><input type="checkbox" class="checkbox-input"><span class="checkbox-tile"><span class="checkbox-icon"><img src="{{ asset('images/office-tools.svg') }}"></span><span class="checkbox-label">ست اداری</span></span></label></div>
+                    </div>
+                    <div class="form-group"><textarea name="fast-order-description" cols="30" rows="10" class="form-control" placeholder="توضیحات"></textarea></div>
+                    <button type="submit" class="btn">ثبت سفارش</button>
+                </form>
+            </div>
+            <div class="float-left col-12 col-md-3 offset-md-1 mt-5 fast-order-guide">
+                <h3 class="mb-5">فرم سفارش فوری</h3>
+                <p class="font-18">با پر کردن این فرم و ثبت نوع طراحی کورد نیاز خود منتظر تماس کارشناسان باشید.</p>
+                <br>
+                <h5 class="font-18">راهنمای ثبت فرم:</h5>
+                <ul class="font-18">
+                    <li>وارد کردن مشخصات شخصی</li>
+                    <li>وارد کردن شماره تماس در دسترس</li>
+                    <li>انتخاب درسته مورد نیاز برای طراحی</li>
+                </ul>
+                <br><br>
+                <p class="font-16 text-secondary">توجه داشته باشید حداقل زمان تماس کارشناسان با شما بعد از ثبت سفارش بین ۱۲ الی ۲۴ ساعت میباشد.</p>
             </div>
         </div>
     </div>
