62a545de68228c5b0952320b1624cf4adee4a40f
Yoonus <yoonustehrani28@gmail.com>
2021-06-21 16:40:24 +0430

    footer is tablet responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -453,11 +453,11 @@
                 <p class="ml-2 text-light">DAYAARTZ</p>
             </div>
             <div class="col-12 float-left px-3 footer-info-container">
-                <div class="col-lg-3 col-md-4 col-12">
+                <div class="col-lg-3 col-md-6 col-12">
                     <p class="title">درباره ما</p>
                     <div><p>گروه طراحی دایا متشکل از تیمی با تجربه و حرفه ای در زمینه های طراحی گرافیـکی و تبلیغاتی در تلاش است بستر مناسبی را برای کسانی که نیازمند طراحی یا طراحی با کیفیت و در سطح بین المللی و همچنین درامدزایی برای طراحان حرفه ای و درجه یک کشور ایران ایجاد سازد.</p></div>
                 </div>
-                <div class="col-lg-2 col-md-2 col-12">
+                <div class="col-lg-2 col-md-6 col-12">
                     <p class="title">لینک های سریع</p>
                     <div>
                         <ul class="text-right p-0">
@@ -469,7 +469,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-2 col-12">
+                <div class="col-lg-2 col-md-6 col-12">
                     <p class="title">پشتیبانی</p>
                     <div>
                         <ul class="text-right p-0 mb-4">
@@ -480,7 +480,7 @@
                         <img src="{{ asset('images/gallery/etemad-namad.png') }}" alt="etemead" class="float-right">
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-4 col-12">
+                <div class="col-lg-3 col-md-6 col-12">
                     <p class="title">دایا مجازی</p>
                     <div class="mb-4 social-media-rows">
                         <div class="social-media-container">
