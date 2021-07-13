37ecd26cad3df71996c8ed13d28fa2373506970a
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-07-13 22:13:08 +0430

    finish order steps and make it responsive

--- resources/views/pages/services/show.blade.php
+++ resources/views/pages/services/show.blade.php
@@ -27,7 +27,7 @@
             <p class="mb-0">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای </p>
             <br>
         </div>
-        <div class="header-vector col-12 col-md-5 mb-3 mb-md-0 vertical-centerlize">
+        <div class="header-vector col-10 col-sm-8 col-md-5 mb-3 mb-md-0 vertical-centerlize offset-sm-2 offset-1 offset-md-0">
             <img src="{{ asset('images/benefits.svg') }}" alt="rules vector" class="rules">
             <img src="{{ asset('images/benefits-background.svg') }}" alt="rules-back" class="rules-back">
         </div>
@@ -48,6 +48,10 @@
                     <span class="step-icon"><i class="fas fa-bullseye"></i></span>
                 </div>
                 <div class="match-line"></div>
+                <div class="step-info step-1 step-info-b d-md-none">
+                    <h4>ثبت سفارش</h4>
+                    <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز</p>
+                </div>
             </div>
             <div class="step-item b-0 l-25 step-2">
                 <div class="step-shape">
@@ -55,6 +59,10 @@
                     <span class="step-icon"><i class="fas fa-handshake"></i></span>
                 </div>
                 <div class="match-line count-clock"></div>
+                <div class="step-info step-2 step-info-t d-md-none">
+                    <h4>تایید تیم دایا</h4>
+                    <p>تایید سفارش ، ایجاد قرارداد بین طرفین و پرداخت پیش پرداخت</p>
+                </div>
             </div>
             <div class="step-item t-0 l-50 step-3">
                 <div class="step-shape">
@@ -62,6 +70,10 @@
                     <span class="step-icon"><i class="fas fa-dollar-sign"></i></span>
                 </div>
                 <div class="match-line"></div>
+                <div class="step-info step-3 step-info-b d-md-none">
+                    <h4>دریافت اتود ها</h4>
+                    <p>تحویل اتود و طرح ها بعد از مدت زمان مقرر شده ای که در زمان ایجاد پروژه محاسبه گردیده</p>
+                </div>
             </div>
             <div class="step-item b-0 l-75 step-4">
                 <div class="step-shape">
@@ -69,34 +81,42 @@
                     <span class="step-icon"><i class="fas fa-cog"></i></span>
                 </div>
                 <div class="match-line count-clock"></div>
+                <div class="step-info step-4 step-info-t d-md-none">
+                    <h4>ادیت و تغییرات</h4>
+                    <p>انجام ادیت ها و تغییرات شما و نزدیک شدن پروژه به طرح مد نظر شما توسط طراحان دایا</p>
+                </div>
             </div>
             <div class="step-item t-0 r-0 step-5">
                 <div class="step-shape">
                     <span class="step-index">05</span>
                     <span class="step-icon"><i class="fas fa-check-square"></i></span>
                 </div>
+                <div class="step-info step-5 step-info-b d-md-none">
+                    <h4>تصویه و تحویل</h4>
+                    <p>پرداخت کامل هزینه سفارش و دریافت فایل مورد نظر شما</p>
+                </div>
             </div>
         </div>
-        <div class="steps-info-container w-100 mb-5">
+        <div class="steps-info-container w-100 d-none d-md-flex">
             <div class="step-info step-1 step-info-b">
                 <h4>ثبت سفارش</h4>
-                <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز.</p>
+                <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز</p>
             </div>
             <div class="step-info step-2 step-info-t">
-                <h4>ثبت سفارش</h4>
-                <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز.</p>
+                <h4>تایید تیم دایا</h4>
+                <p>تایید سفارش ، ایجاد قرارداد بین طرفین و پرداخت پیش پرداخت</p>
             </div>
             <div class="step-info step-3 step-info-b">
-                <h4>ثبت سفارش</h4>
-                <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز.</p>
+                <h4>دریافت اتود ها</h4>
+                <p>تحویل اتود و طرح ها بعد از مدت زمان مقرر شده ای که در زمان ایجاد پروژه محاسبه گردیده</p>
             </div>
             <div class="step-info step-4 step-info-t">
-                <h4>ثبت سفارش</h4>
-                <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز.</p>
+                <h4>ادیت و تغییرات</h4>
+                <p>انجام ادیت ها و تغییرات شما و نزدیک شدن پروژه به طرح مد نظر شما توسط طراحان دایا</p>
             </div>
             <div class="step-info step-5 step-info-b">
-                <h4>ثبت سفارش</h4>
-                <p>ایجاد سفارش از طریق روش های ثبت سفارش درج شده در صفحه ، روش های ثبت سفارش دایا آرتز.</p>
+                <h4>تصویه و تحویل</h4>
+                <p>پرداخت کامل هزینه سفارش و دریافت فایل مورد نظر شما</p>
             </div>
         </div>
     </div>
