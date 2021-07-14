4e0a246ce6450f41a66cb06f6d60cc443cb18706
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-07-14 09:51:55 +0430

    service page designing .....70%

--- resources/views/pages/services/show.blade.php
+++ resources/views/pages/services/show.blade.php
@@ -121,4 +121,13 @@
         </div>
     </div>
     <!-- order steps -->
+    <!-- start order 1 -->
+    <div class="section w-100 start-order-section">
+        <div class="start-order-1 w-100 p-5">
+            <h4>برای شروع روند سفارش کلیک کنید</h4>
+            <button class="btn btn-dark badge-pill mt-4">شروع سفارش</button>
+        </div>
+    </div>
+    <!-- end start order 1 -->
+    
 @endsection
