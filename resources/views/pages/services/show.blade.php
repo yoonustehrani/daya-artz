733ec57a2bdbd81a1a362f38ec69adba6cb58241
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2021-07-29 13:32:58 +0430

    add slider for blog suggestions

--- resources/views/pages/services/show.blade.php
+++ resources/views/pages/services/show.blade.php
@@ -409,47 +409,6 @@
     </div>
     <!-- end FAQ accordion 1 -->
     <!-- daya blog -->
-    <div class="section w-100 mt-5">
-        <div class="title-section col-12 mb-3">
-            <div class="title-container">
-                <h2 class="title-text">پست های پیشنهادی</h2>
-            </div>
-        </div>
-        <div class="blog-posts-container w-100 text-center">
-            <article class="blog-post col-12 col-md-3">
-                <a href="#">
-                    <div class="img-container w-100">
-                        <img src="{{ asset('images/gallery/blog-post-1.jpg') }}" alt="post-cover" class="w-100 h-100">
-                        <span>19 <br> خرداد</span>
-                    </div>
-                    <p class="blog-post-source">نوشته شده توسط: ویکی پدیا</p>
-                    <h3 class="title block">چگونه لوگوی خوب را بشناسیم</h3>
-                    <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
-                </a>
-            </article>
-            <article class="blog-post col-12 col-md-3">
-                <a href="#">
-                    <div class="img-container w-100">
-                        <img src="{{ asset('images/gallery/blog-post-2.jpg') }}" alt="post-cover" class="w-100 h-100">
-                        <span>04 <br> تیر</span>
-                    </div>
-                    <p class="blog-post-source">نوشته شده توسط: تیم دایا</p>
-                    <h3 class="title block">بهترین لوگوهای جهان</h3>
-                    <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
-                </a>
-            </article>
-            <article class="blog-post col-12 col-md-3">
-                <a href="#">
-                    <div class="img-container w-100">
-                        <img src="{{ asset('images/gallery/blog-post-3.jpg') }}" alt="post-cover" class="w-100 h-100">
-                        <span>30 <br> مهر</span>
-                    </div>
-                    <p class="blog-post-source">نوشته شده توسط: تیم دایا</p>
-                    <h3 class="title block">درباره برندینگ چه میدانید؟</h3>
-                    <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
-                </a>
-            </article>
-        </div>
-    </div>
+    @include('components.blog-suggestion')
     <!-- end daya blog -->
 @endsection
