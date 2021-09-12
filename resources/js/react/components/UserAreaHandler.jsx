59a458020e52e247ab6384478e0110f16ab396be
yoonus <yoonustehrani28@gmail.com>
2021-09-12 20:59:54 +0430

    some new changes

--- resources/js/react/components/UserAreaHandler.jsx
+++ resources/js/react/components/UserAreaHandler.jsx
@@ -4,7 +4,7 @@ export default class UserAreaHandler extends Component {
     render() {
         return (
             <div>
-                <p>hello</p>
+                hello
             </div>
         )
     }
