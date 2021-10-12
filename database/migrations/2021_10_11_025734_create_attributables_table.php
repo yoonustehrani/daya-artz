1aeac5aa35619a9276998905d40c65fa5dad5af4
Yoonus <yoonustehrani28@gmail.com>
2021-10-12 04:13:37 +0330

    attributable migration

---
+++ database/migrations/2021_10_11_025734_create_attributables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributables', function (Blueprint $table) {
+            $table->foreignId('attribute_id');
+            $table->morphs('attributable');
+            $table->json('details')->default(json_encode(json_decode('[]')));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributeables');
+    }
+}
