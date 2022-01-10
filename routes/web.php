cd7b9302ad053cc99a408fbeb2f81298279cc13f
yoonus <yoonustehrani28@gmail.com>
2022-01-10 10:45:53 +0330

    code cleaning

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function() {
-//     $user = User::first();
-//     // $verification = "";
-//     // return $user->generateVerificationCode('verify_phone', generate_code(), false, 2);
-//     // event(new UserRegistered($user));
-//     return $user;
-// });
-
 Route::get('/', [WebsiteController::class, 'index'])->name('home');
 
 Route::get('services', [WebsiteController::class, 'services'])->name('services.index');
@@ -42,16 +34,6 @@ Route::view('contact', 'pages.contact')->name('contact');
 Route::view('policy', 'pages.policy')->name('policy');
 
 Route::view('userarea/{path?}', 'pages.userarea')->where('path', '.*')->name('userarea');
-// Auth::routes([
-//     'register' => false,
-//     // 'login' => false,
-//     'verify' => true
-// ]);
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-
-// Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::get('email/verify/{id}/{hash}', function($id, $hash, Request $request) {
@@ -68,19 +50,9 @@ Route::get('email/verify/{id}/{hash}', function($id, $hash, Request $request) {
     return redirect()->to(route('userarea'));
 })->middleware('signed')->name('verification.email.verify');
 
+/* 
 Route::get('test', function () {
     $user = User::find(2);
     $user->notifyNow(new VerificationNotification([SMSChannel::class]));
-    // $json_string = '{"name": "yoonus", "age": 18}';
-    // dd(json_decode($json_string));
-    // $offers = App\Offer::limit(3)->get();
-    // return $offers;
-    // $company = Company::first();
-    // $company->load('business_type', 'product_type');
-    // return $company;
 });
-
-Route::get('testme', function () {
-    return 'hello';
-    // return $request;
-});
+*/
