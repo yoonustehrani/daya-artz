8b7c4d165fae2349b1481a302964f1334acf2942
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2022-01-18 01:40:04 +0330

    new changes in blog page index

--- resources/views/pages/posts/index.blade.php
+++ resources/views/pages/posts/index.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.page')
 
 @section('header')
-<div class="header-section dotted-background policy-header">
-    <div class="header-title float-right h-100 p-4 col-lg-6 col-md-7 col-12">
-        <h1 class="w-100 title text-center">دایا <span class="aqua">بلاگ</span></h1>
+<div class="header-section dotted-background blog-header">
+    <div class="float-right p-4 col-lg-6 col-md-7 col-12 my-auto">
+        <div class="header-title">
+            <h1 class="w-100 title text-center">دایا <span class="aqua">بلاگ</span></h1>
+        </div>
+        <div class="input-group input-group-lg search-box">
+            <input class="h-100 form-control eggplant" type="text" placeholder="دنبال چه مطلبی هستی؟">
+            <div class="input-group-prepend">
+                <button class="btn btn-search" type="button"><i class="fas fa-search"></i></button>
+            </div>
+        </div>
     </div>
     <div class="header-vector col-lg-6 col-md-5 col-12 mb-3 mb-md-0">
         <img src="{{ asset('images/blog.svg') }}">
@@ -13,15 +21,6 @@
 @endsection
 
 @section('content')
-<div class="section dotted-background bg-light col-12 p-4">
-    <p class="title title-bigger eggplant-light text-center">دنبال چه مطلبی هستی ؟</p>
-    <div class="input-group input-group-lg search-box col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-12 float-left">
-        <div class="input-group-prepend">
-            <button class="btn btn-search" type="button"><i class="fas fa-search"></i></button>
-        </div>
-        <input class="h-100 form-control eggplant" type="text" placeholder="اینجا بنویس">
-    </div>
-</div>
 <div class="section col-12 mt-3">
     <div class="title-section col-12 mb-3">
         <div class="title-container">
@@ -29,38 +28,47 @@
             <span class="title-underline"></span>
         </div>
     </div>
-    <div class="blog-posts-container w-100 text-center">
-        <article class="blog-post col-12 col-md-3">
+    <div class="blog-posts-container text-center">
+        <article class="blog-post col-12 col-md-6 col-lg-4">
             <a href="{{ route('blog.show', ['title' => 'چگونه لوگوی خوب را بشناسیم']) }}">
                 <div class="img-container w-100">
                     <img src="{{ asset('images/gallery/blog-post-1.jpg') }}" alt="post-cover" class="w-100 h-100">
-                    <span>19 <br> خرداد</span>
+                    <span class="article-date">19 فروردین <br> 1400</span>
                 </div>
-                <p class="blog-post-source">نوشته شده توسط: ویکی پدیا</p>
-                <h3 class="block">چگونه لوگوی خوب را بشناسیم</h3>
-                <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
+                <div class="article-info">
+                    <p class="blog-post-source">نوشته شده توسط: ویکی پدیا</p>
+                    <h3 class="block">چگونه لوگوی خوب را بشناسیم</h3>
+                    <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
+                    <div class="article-time"><span>زمان مطالعه: 5 دقیقه</span></div>
+                </div>
             </a>
         </article>
-        <article class="blog-post col-12 col-md-3">
+        <article class="blog-post col-12 col-md-6 col-lg-4">
             <a href="{{ route('blog.show', ['title' => 'بهترین لوگوهای جهان']) }}">
                 <div class="img-container w-100">
                     <img src="{{ asset('images/gallery/blog-post-2.jpg') }}" alt="post-cover" class="w-100 h-100">
-                    <span>04 <br> تیر</span>
+                    <span class="article-date">19 فروردین <br> 1400</span>
                 </div>
-                <p class="blog-post-source">نوشته شده توسط: تیم دایا</p>
-                <h3 class="block">بهترین لوگوهای جهان</h3>
-                <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
+                <div class="article-info">
+                    <p class="blog-post-source">نوشته شده توسط: تیم دایا</p>
+                    <h3 class="block">بهترین لوگوهای جهان</h3>
+                    <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
+                    <div class="article-time"><span>زمان مطالعه: 5 دقیقه</span></div>
+                </div>
             </a>
         </article>
-        <article class="blog-post col-12 col-md-3">
+        <article class="blog-post col-12 col-md-6 col-lg-4">
             <a href="{{ route('blog.show', ['title' => 'درباره برندینگ چه میدانید؟']) }}">
                 <div class="img-container w-100">
                     <img src="{{ asset('images/gallery/blog-post-3.jpg') }}" alt="post-cover" class="w-100 h-100">
-                    <span>30 <br> مهر</span>
+                    <span class="article-date">19 فروردین <br> 1400</span>
                 </div>
-                <p class="blog-post-source">نوشته شده توسط: تیم دایا</p>
-                <h3 class="block">درباره برندینگ چه میدانید؟</h3>
-                <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
+                <div class="article-info">
+                    <p class="blog-post-source">نوشته شده توسط: تیم دایا</p>
+                    <h3 class="block">درباره برندینگ چه میدانید؟</h3>
+                    <p class="blog-post-description text-secondary">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا </p>
+                    <div class="article-time"><span>زمان مطالعه: 5 دقیقه</span></div>
+                </div> 
             </a>
         </article>
     </div>
