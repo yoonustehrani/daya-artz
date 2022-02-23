2c1786618ca1876dd8aa982bb4c9058e4c0cdfda
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2022-02-24 02:25:03 +0330

    add tailwind and designing landing pages

--- resources/views/pages/landing/opening.blade.php
+++ resources/views/pages/landing/opening.blade.php
@@ -1,4 +1,3 @@
 <x-layout.landing :title="$page->title">
-{{-- this component is extending from `resources/views/components/landing.blade.php` --}}
-{{-- Content goes here --}}
+    
 </x-layout.landing>
