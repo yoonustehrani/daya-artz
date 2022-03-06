a711cddcc6d8ab526725b1e5061d96a651232aef
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2022-03-06 07:17:54 +0330

    design order item page

--- public/js/resources_js_react_Pages_Private_Orders_OrderItemPage_jsx.js
+++ public/js/resources_js_react_Pages_Private_Orders_OrderItemPage_jsx.js
@@ -1,6 +1,37 @@
 "use strict";
 (self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_react_Pages_Private_Orders_OrderItemPage_jsx"],{
 
+/***/ "./resources/js/react/Pages/Private/Layout/components/Loading.jsx":
+/*!************************************************************************!*\
+  !*** ./resources/js/react/Pages/Private/Layout/components/Loading.jsx ***!
+  \************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Loading)
+/* harmony export */ });
+/* harmony import */ var react_activity__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-activity */ "./node_modules/react-activity/dist/react-activity.js");
+/* harmony import */ var react_activity__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_activity__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+function Loading(props) {
+  var _props$size;
+
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+    className: "activity-container mb-2",
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(react_activity__WEBPACK_IMPORTED_MODULE_1__.Spinner, {
+      color: "#999999",
+      size: (_props$size = props.size) !== null && _props$size !== void 0 ? _props$size : 34,
+      speed: 0.5,
+      animating: true
+    })
+  });
+}
+
+/***/ }),
+
 /***/ "./resources/js/react/Pages/Private/Orders/OrderItemPage.jsx":
 /*!*******************************************************************!*\
   !*** ./resources/js/react/Pages/Private/Orders/OrderItemPage.jsx ***!
@@ -14,7 +45,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var _Layout_components_Loading__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../Layout/components/Loading */ "./resources/js/react/Pages/Private/Layout/components/Loading.jsx");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
 
@@ -44,6 +76,9 @@ function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Re
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+
 
 
 
@@ -80,7 +115,7 @@ var OrderItemPage = /*#__PURE__*/function (_Component) {
 
     _this.state = {
       orderItem: null,
-      loading: true
+      loading: false
     };
     return _this;
   }
@@ -93,8 +128,210 @@ var OrderItemPage = /*#__PURE__*/function (_Component) {
   }, {
     key: "render",
     value: function render() {
-      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
-        children: "OrderItemPage"
+      var loading = this.state.loading;
+      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+        children: loading ? /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_Layout_components_Loading__WEBPACK_IMPORTED_MODULE_2__["default"], {}) : /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.Fragment, {
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+            className: "order-item-header",
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+              className: "order-item-header-title",
+              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h4", {
+                children: "\u0633\u0641\u0627\u0631\u0634 \u0644\u0648\u06AF\u0648\u06CC \u0627\u0641\u062A\u062A\u0627\u062D\u06CC\u0647 \u0631\u0633\u062A\u0648\u0631\u0627\u0646 \u0627\u062D\u0633\u0627\u0646"
+              })
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+              className: "progress",
+              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+                className: "progress-bar w-25",
+                role: "progressbar",
+                "aria-valuenow": "25%",
+                "aria-valuemin": "0%",
+                "area-valuemax": "100%",
+                children: "25%"
+              })
+            })]
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+            className: "order-item-infos-container",
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-item-section",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-item-section-icon",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-info-square"
+                })
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("ul", {
+                className: "order-item-list",
+                children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u0646\u0648\u0639 \u0637\u0631\u0627\u062D\u06CC: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "\u0637\u0631\u0627\u062D\u06CC \u0644\u0648\u06AF\u0648\u06CC \u062A\u0635\u0648\u06CC\u0631\u06CC"
+                  })]
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u0633\u0641\u0627\u0631\u0634: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "#fs2l48fk"
+                  })]
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("li", {})]
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-item-section",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-item-section-icon",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-spinner"
+                })
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("ul", {
+                className: "order-item-list",
+                children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u0648\u0636\u0639\u06CC\u062A: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "\u062F\u0631 \u062F\u0633\u062A \u0637\u0631\u0627\u062D\u06CC"
+                  })]
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u062A\u0648\u0636\u06CC\u062D\u0627\u062A: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "\u062F\u0631 \u062D\u0627\u0644 \u062D\u0627\u0636\u0631 2 \u0627\u062A\u0648\u062F \u0627\u0632 3 \u0627\u062A\u0648\u062F \u062F\u0631\u062E\u0648\u0627\u0633\u062A\u06CC \u0634\u0645\u0627 \u062A\u06A9\u0645\u06CC\u0644 \u0634\u062F\u0647."
+                  })]
+                })]
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-item-section",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-item-section-icon",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-calendar-exclamation"
+                })
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("ul", {
+                className: "order-item-list",
+                children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u0645\u0648\u0639\u062F \u062A\u062D\u0648\u06CC\u0644: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "1400/12/24"
+                  })]
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u062A\u0627\u0631\u06CC\u062E \u0627\u062A\u0645\u0627\u0645: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "1401/1/2"
+                  })]
+                })]
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-item-section",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-item-section-icon",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-comment-alt-medical"
+                })
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("ul", {
+                className: "order-item-list",
+                children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u067E\u06A9\u06CC\u062C \u0645\u0631\u0628\u0648\u0637\u0647 :", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "\u0627\u0641\u062A\u062A\u0627\u062D\u06CC\u0647 \u0631\u0633\u062A\u0648\u0631\u0627\u0646"
+                  })]
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+                  children: ["\u062A\u0648\u0636\u06CC\u062D\u0627\u062A \u067E\u0631\u0648\u0698\u0647 :", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                    children: "\u0627\u06CC\u0646 \u067E\u0631\u0648\u0698\u0647 \u0632\u06CC\u0631 \u0646\u0638\u0631 \u0637\u0631\u0627\u062D\u0627\u0646 \u0645\u062A\u062E\u0635\u0635 \u062F\u0627\u06CC\u0627 \u062F\u0631 \u062F\u0633\u062A \u0627\u0646\u062C\u0627\u0645 \u0627\u0633\u062A \u0648 \u062F\u0648\u0644 \u0627\u0642\u0627 \u0628\u0632\u0631\u06AF \u0645\u0646 \u06A9\u0648\u0686\u06CC\u06A9\u0647"
+                  })]
+                })]
+              })]
+            })]
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+            className: "order-item-progress-container level-4",
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-level-progress",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h4", {
+                className: "order-level-title",
+                children: "\u062B\u0628\u062A \u0633\u0641\u0627\u0631\u0634"
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-level-circle",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-user-edit"
+                })
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-level-progress",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h4", {
+                className: "order-level-title",
+                children: "\u062A\u0645\u0627\u0633 \u062A\u0644\u0641\u0646\u06CC"
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-level-circle",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-phone-volume"
+                })
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-level-progress",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h4", {
+                className: "order-level-title",
+                children: "\u067E\u0631\u062F\u0627\u062E\u062A \u0628\u06CC\u0639\u0627\u0646\u0647"
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-level-circle",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-credit-card"
+                })
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-level-progress",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h4", {
+                className: "order-level-title",
+                children: "\u0637\u0631\u0627\u062D\u06CC \u0633\u0641\u0627\u0631\u0634"
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-level-circle",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-drafting-compass"
+                })
+              })]
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+              className: "order-level-progress",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("h4", {
+                className: "order-level-title",
+                children: "\u062A\u0633\u0648\u06CC\u0647 \u0648 \u062A\u062D\u0648\u06CC\u0644"
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("span", {
+                className: "order-level-circle",
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("i", {
+                  className: "far fa-handshake"
+                })
+              })]
+            })]
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            className: "w-100 table-responsive",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("table", {
+              className: "table table-hover",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("caption", {
+                children: "\u062A\u06CC\u06A9\u062A \u0647\u0627\u06CC \u0645\u0631\u062A\u0628\u0637 \u0628\u0647 \u0627\u06CC\u0646 \u0633\u0641\u0627\u0631\u0634"
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("thead", {
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("tr", {
+                  children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "#"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "\u0634\u0645\u0627\u0631\u0647 \u062A\u06CC\u06A9\u062A"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "\u0639\u0646\u0648\u0627\u0646 \u062A\u06CC\u06A9\u062A"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "\u0628\u062E\u0634 \u0645\u0631\u0628\u0648\u0637\u0647"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "\u0648\u0636\u0639\u06CC\u062A"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "\u062A\u0627\u0631\u06CC\u062E \u0627\u06CC\u062C\u0627\u062F"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "\u062A\u0627\u0631\u06CC\u062E \u0627\u062A\u0645\u0627\u0645"
+                  })]
+                })
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("tbody", {
+                children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("tr", {
+                  children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+                    children: "1"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+                    children: "#46753"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+                    children: "\u0648\u06CC\u0631\u0627\u06CC\u0634"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+                    children: "\u0627\u0645\u0648\u0631 \u0637\u0631\u0627\u062D\u06CC"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+                    children: "\u0628\u0633\u062A\u0647 \u0634\u062F\u0647"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+                    children: "1400/12/5"
+                  }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+                    children: "1400/12/12"
+                  })]
+                })
+              })]
+            })
+          })]
+        })
       });
     }
   }]);
--- resources/js/react/Pages/Private/Orders/OrderItemPage.jsx
+++ resources/js/react/Pages/Private/Orders/OrderItemPage.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Loading from '../Layout/components/Loading'
 
 export default class OrderItemPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
             orderItem: null,
-            loading: true
+            loading: false
         }
     }
     componentDidMount() {
@@ -16,8 +17,101 @@ export default class OrderItemPage extends Component {
         console.log(itemId);
     }
     render() {
+        let { loading } = this.state
         return (
-            <div>OrderItemPage</div>
+            <div>
+                {loading ? <Loading />
+                : <>
+                    <div className='order-item-header'>
+                        <div className="order-item-header-title"><h4>سفارش لوگوی افتتاحیه رستوران احسان</h4></div>
+                        <div className="progress">
+                            <div className="progress-bar w-25" role="progressbar" aria-valuenow="25%" aria-valuemin="0%" area-valuemax="100%">25%</div>
+                        </div>
+                        {/* <span className='order-item-header-icon'><i className='far fa-drafting-compass'></i></span> */}
+                    </div>
+                    <div className='order-item-infos-container'>
+                        <div className='order-item-section'>
+                            <span className='order-item-section-icon'><i className='far fa-info-square'></i></span>
+                            <ul className='order-item-list'>
+                                <li>نوع طراحی: <span>طراحی لوگوی تصویری</span></li>
+                                <li>سفارش: <span>#fs2l48fk</span></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div className='order-item-section'>
+                            <span className='order-item-section-icon'><i className="far fa-spinner"></i></span>
+                            <ul className='order-item-list'>
+                                <li>وضعیت: <span>در دست طراحی</span></li>
+                                <li>توضیحات: <span>در حال حاضر 2 اتود از 3 اتود درخواستی شما تکمیل شده.</span></li>
+                            </ul>
+                        </div>
+                        <div className='order-item-section'>
+                            <span className='order-item-section-icon'><i className="far fa-calendar-exclamation"></i></span>
+                            <ul className='order-item-list'>
+                                <li>موعد تحویل: <span>1400/12/24</span></li>
+                                <li>تاریخ اتمام: <span>1401/1/2</span></li>
+                            </ul>
+                        </div>
+                        <div className='order-item-section'>
+                            <span className='order-item-section-icon'><i className="far fa-comment-alt-medical"></i></span>
+                            <ul className='order-item-list'>
+                                <li>پکیج مربوطه :<span>افتتاحیه رستوران</span></li>
+                                <li>توضیحات پروژه :<span>این پروژه زیر نظر طراحان متخصص دایا در دست انجام است و دول اقا بزرگ من کوچیکه</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="order-item-progress-container level-4">
+                        <div className='order-level-progress'>
+                            <h4 className='order-level-title'>ثبت سفارش</h4>
+                            <span className='order-level-circle'><i className='far fa-user-edit'></i></span>
+                        </div>
+                        <div className='order-level-progress'>
+                            <h4 className='order-level-title'>تماس تلفنی</h4>
+                            <span className='order-level-circle'><i className='far fa-phone-volume'></i></span>
+                        </div>
+                        <div className='order-level-progress'>
+                            <h4 className='order-level-title'>پرداخت بیعانه</h4>
+                            <span className='order-level-circle'><i className='far fa-credit-card'></i></span>
+                        </div>
+                        <div className='order-level-progress'>
+                            <h4 className='order-level-title'>طراحی سفارش</h4>
+                            <span className='order-level-circle'><i className='far fa-drafting-compass'></i></span>
+                        </div>
+                        <div className='order-level-progress'>
+                            <h4 className='order-level-title'>تسویه و تحویل</h4>
+                            <span className='order-level-circle'><i className='far fa-handshake'></i></span>
+                        </div>
+                    </div>
+                    <div className="w-100 table-responsive">
+                        <table className='table table-hover'>
+                            <caption>تیکت های مرتبط به این سفارش</caption>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>شماره تیکت</th>
+                                    <th>عنوان تیکت</th>
+                                    <th>بخش مربوطه</th>
+                                    <th>وضعیت</th>
+                                    <th>تاریخ ایجاد</th>
+                                    <th>تاریخ اتمام</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <td>#46753</td>
+                                    <td>ویرایش</td>
+                                    <td>امور طراحی</td>
+                                    <td>بسته شده</td>
+                                    <td>1400/12/5</td>
+                                    <td>1400/12/12</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </> 
+                }
+            </div>
         )
     }
 }
