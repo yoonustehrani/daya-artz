dba8ae00dd04b471944105dbff73bb0075e0d536
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2022-03-18 15:08:49 +0330

    branch site merged with public

--- resources/js/components/scrollTo.js
+++ resources/js/components/scrollTo.js
@@ -1,13 +1,15 @@
 // scroll to quick order in home page
-let quickOrderForm = $("#quick-order-container"),
-homePageBtn = $("#scroll-to-form-btn"),
-targetPosition = quickOrderForm.offset().top + $("body").scrollTop() - 50,
-onClickBtn = () => {
-    $("body").animate({
-        scrollTop: targetPosition
-    }, 500)
+let quickOrderForm = $("#quick-order-container")
+if (quickOrderForm.length > 0) {
+    let homePageBtn = $("#scroll-to-form-btn"),
+    targetPosition = quickOrderForm.offset().top + $("body").scrollTop(),
+    onClickBtn = () => {
+        $("body").animate({
+            scrollTop: targetPosition
+        }, 500)
+    }
+    homePageBtn.on("click", onClickBtn)
+    $(".main-services-container, .service-groups-container").find('.scroll-to-form').each((i, elem) => {
+        $(elem).on("click", onClickBtn)
+    })
 }
-homePageBtn.on("click", onClickBtn)
-$(".main-services-container, .service-groups-container").find('.scroll-to-form').each((i, elem) => {
-    $(elem).on("click", onClickBtn)
-})
