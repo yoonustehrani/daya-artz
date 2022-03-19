8857d391ef8786fb4cb6484b53b1e0441c90e9dc
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2022-03-19 07:23:10 +0330

    preload iransans font

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="preload" as="font" href="{{ asset("fonts/IRANSansWeb.ttf") }}" type="font/ttf" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title') | دایا آرتز</title>
     @stack('head')
