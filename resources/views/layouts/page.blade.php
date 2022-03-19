c0386c2c2afa65464315eb0413632cc27f7a1b6a
yoonus <yoonustehrani28@gmail.com>
2022-03-19 07:22:18 +0330

    font awesome cdn

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     @stack('head')
 </head>
 <body>
