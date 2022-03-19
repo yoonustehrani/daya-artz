368844aacaebb6ff7dd004ff664d8d16329102bb
yoonus <yoonustehrani28@gmail.com>
2022-03-19 08:56:41 +0330

    font without preload

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <link rel="preload" as="font" href="{{ asset("fonts/IRANSansWeb.ttf") }}" type="font/ttf" crossorigin="anonymous"> --}}
-    <link rel="preload" as="style" href="{{ asset("css/_fonts.css") }}">
-    {{-- <link rel="stylesheet" href="{{ asset("css/_fonts.css") }}"> --}}
+    {{-- <link rel="preload" as="style" href="{{ asset("css/_fonts.css") }}"> --}}
+    <link rel="stylesheet" href="{{ asset("css/_fonts.css") }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" media="all" rel="stylesheet"> --}}
     <title>@yield('title') | دایا آرتز</title>
