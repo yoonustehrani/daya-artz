d3b2f5802fee6902a1210bafec14f99648fe7889
yoonus <yoonustehrani28@gmail.com>
2022-04-02 22:29:50 +0430

    route parameter bug fixed

--- resources/views/custom/tickets.blade.php
+++ resources/views/custom/tickets.blade.php
@@ -5,8 +5,8 @@
         class="h-[calc(100%-36px)] pt-4"
         data-departments="{{ route('api.zeus.custom.tickets.departments') }}"
         data-tickets="{{ route('api.zeus.custom.tickets.index', ['department' => 'departmentId']) }}"
-        data-ticket="{{ route('api.zeus.custom.tickets.show') }}"
-        data-message="{{ route('api.zeus.custom.tickets.message') }}"></div>
+        data-ticket="{{ route('api.zeus.custom.tickets.show', ['ticket' => 'ticketId']) }}"
+        data-message="{{ route('api.zeus.custom.tickets.message', ['ticket' => 'ticketId']) }}"></div>
     @push('scripts')
         <script src="{{ asset('js/zeusTickets.js') }}"></script>
     @endpush
