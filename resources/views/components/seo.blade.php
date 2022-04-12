c3518bd5be3cfb246bd7b473ec2642aa48ade81e
yoonus <yoonustehrani28@gmail.com>
2022-04-12 07:20:58 +0430

    seo bug fixed

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -1,23 +1,22 @@
 @php
-    $seo = get_seo_settings($slug, $instance);
+    $seo = isset($instance, $slug) ? get_seo_settings($slug, $instance) : null;
 @endphp
-@if (isset($instance, $slug))
-    @if ($seo)
-        <meta name="description" content="{{ $seo->description }}">
-    @endif
+<meta name="robots" content="index, follow, max-image-preview:standard, max-snippet:-1, max-video-preview:-1"/>
+@if ($seo)
+    <meta name="description" content="{{ $seo->description }}"/>
 @endif
-<meta property="og:locale" content="fa_IR">
-    <meta property="og:site_name" content="دایا آرتز">
-    <meta property="og:url" content="{{ request()->url() }}">
+    <meta property="og:locale" content="fa_IR"/>
+    <meta property="og:site_name" content="دایا آرتز"/>
+    <meta property="og:url" content="{{ request()->url() }}"/>
 @isset($og)
-    <meta property="og:type" content="{{ $og['type'] ?? 'website' }}">
-    <meta property="og:title" content="{{ $og['title'] ?? '' }}">
+    <meta property="og:type" content="{{ $og['type'] ?? 'website' }}"/>
+    <meta property="og:title" content="{{ $og['title'] ?? '' }}"/>
     @if ($seo)
-    <meta property="og:description" content="{{ $seo->description }}">
+    <meta property="og:description" content="{{ $seo->description }}"/>
     @endif
     @isset ($og['type_details'])
         @foreach ($og['type_details'] as $key => $item)
-            <meta property="{{ $og['type'] }}:{{ $key }}" content="{{ $item }}">
+            <meta property="{{ $og['type'] }}:{{ $key }}" content="{{ $item }}"/>
         @endforeach
     @endisset
     @isset ($og['image'])
