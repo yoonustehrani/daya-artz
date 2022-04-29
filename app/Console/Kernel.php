780e815024b8457058aa0f4db7dffd648093e8bf
yoonus <yoonustehrani28@gmail.com>
2022-04-29 14:32:16 +0430

    cronjob changetime

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:database')->timezone('Asia/Tehran')->hourlyAt(0);
+        $schedule->command('backup:database')->timezone('Asia/Tehran')->hourly();
     }
 
     /**
