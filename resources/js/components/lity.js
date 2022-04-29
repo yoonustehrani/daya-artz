141fb098e791cfca63d25fccfcbbebccddbd8156
Amir Reza Mansoorian <amirreza.mansoorian2003@gmail.com>
2022-04-29 10:03:55 +0430

    fix merging bug and production

--- resources/js/components/lity.js
+++ resources/js/components/lity.js
@@ -1,6 +1,9 @@
 import 'lity/dist/lity'
 
 // for adding lity to blog post images
-$("#post-content img").each((index, elem) => {
-    $(elem).wrap(`<a href=${$(elem).attr("src")} data-lity></a>`)
+$(() => {
+    $("#post-content img").each((index, elem) => {
+        let src = $(elem).attr("data-src") ?? $(elem).attr("src");
+        $(elem).wrap(`<a href=${src} data-lity></a>`)
+    })
 })
