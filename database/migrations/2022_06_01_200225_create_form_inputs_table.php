990933737b2c0a4a23ceccd3a49b9e37c490b614
yoonus <yoonustehrani28@gmail.com>
2022-06-01 20:20:07 +0430

    form inputs and answers and submissions migrations are done

---
+++ database/migrations/2022_06_01_200225_create_form_inputs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormInputsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_inputs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_id')->references('id')->on('forms')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('label');
+            $table->string('type');
+            $table->string('default')->nullable();
+            $table->string('placeholder')->nullable();
+            $table->json('details')->nullable();
+            $table->boolean('required')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_inputs');
+    }
+}
---
+++ database/migrations/2022_06_01_201153_create_form_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_id')->references('id')->on('forms')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->bigInteger('user_id')->nullable();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_answers');
+    }
+}
