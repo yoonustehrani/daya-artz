6b8536336f85d559b4ca2989115fe7d3fb4ead21
yoonus <yoonustehrani28@gmail.com>
2022-06-16 02:23:11 +0430

    production

--- webpack.mix.js
+++ webpack.mix.js
@@ -42,20 +42,20 @@ if (mix.inProduction) {
 }
 
 // website
-// mix.sass(res.sass + 'app.scss', pub.css)
-// mix.sass(res.sass + '_fonts.scss', pub.css)
-// mix.js(res.js + 'app.js', pub.js).react()
-// mix.js(res.js + "userarea.js", pub.js).react()
+mix.sass(res.sass + 'app.scss', pub.css)
+mix.sass(res.sass + '_fonts.scss', pub.css)
+mix.js(res.js + 'app.js', pub.js).react()
+mix.js(res.js + "userarea.js", pub.js).react()
 // err
 mix.sass(res.sass + "err.scss", pub.css)
 // catalog
-// mix.postCss(res.css + "catalog.css", pub.css, [
-//     require("postcss-import"),
-//     require('tailwindcss/nesting'),
-//     require('tailwindcss')({config: './tailwind.catalog.config.js'}),
-//     require('autoprefixer')
-// ])
-//     .js(src.res.js + "catalog.js", src.pub.js).react()
+mix.postCss(res.css + "catalog.css", pub.css, [
+    require("postcss-import"),
+    require('tailwindcss/nesting'),
+    require('tailwindcss')({config: './tailwind.catalog.config.js'}),
+    require('autoprefixer')
+])
+    .js(src.res.js + "catalog.js", src.pub.js).react()
 
 // landing
 mix.postCss(res.css + "landing.css", pub.css, [
@@ -66,20 +66,20 @@ mix.postCss(res.css + "landing.css", pub.css, [
     ])
     .js(res.js + "landing-opening.js", pub.js).react()
 // zeus
-// mix.postCss(zeus.res.css + "style.css", zeus.pub.css, [
-//     require('postcss-import'),
-//     require('tailwindcss/nesting'),
-//     require('tailwindcss')({config: "./tailwind.zeus.config.js"}),
-//     require('autoprefixer')
-// ])
-// mix.postCss(zeus.res.css + "rtl.css", zeus.pub.css, [
-//     require('postcss-import'),
-//     require('tailwindcss/nesting'),
-//     require('tailwindcss')({config: './tailwind.zeus.config.js'}),
-//     require('autoprefixer')
-// ])
-// mix.js(zeus.res.js + "app.js", zeus.pub.js).react()
-// mix.js(res.js + "components/zeusTickets.js", pub.js).react()
+mix.postCss(zeus.res.css + "style.css", zeus.pub.css, [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss')({config: "./tailwind.zeus.config.js"}),
+    require('autoprefixer')
+])
+mix.postCss(zeus.res.css + "rtl.css", zeus.pub.css, [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss')({config: './tailwind.zeus.config.js'}),
+    require('autoprefixer')
+])
+mix.js(zeus.res.js + "app.js", zeus.pub.js).react()
+mix.js(res.js + "components/zeusTickets.js", pub.js).react()
 
 // tinymce
 // mix.copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce')
