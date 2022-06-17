9e7abb8ba5a2af27f6b812285258942f9693870e
yoonus <yoonustehrani28@gmail.com>
2022-06-17 15:34:35 +0430

    deleted hotjar script due to sanctions

--- resources/views/components/gstag.blade.php
+++ resources/views/components/gstag.blade.php
@@ -8,7 +8,7 @@ gtag('js', new Date());
 
 gtag('config', 'G-VCLC4ZVHRT');
 </script>
-<!-- Hotjar Tracking Code for https://dayaartz.com -->
+{{-- <!-- Hotjar Tracking Code for https://dayaartz.com -->
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -18,5 +18,5 @@ gtag('config', 'G-VCLC4ZVHRT');
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+</script> --}}
 @endunless
