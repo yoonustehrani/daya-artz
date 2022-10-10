dddb8beb7c9375224b57224384774a102b34ef28
yoonus <yoonustehrani28@gmail.com>
2022-10-11 00:01:04 +0330

    TelegramUser model relations

--- app/Models/TelegramUser.php
+++ app/Models/TelegramUser.php
@@ -13,8 +13,13 @@ class TelegramUser extends Model
     protected $primaryKey = 'chat_id';
     public $incrementing = false;
 
-    // public function orders()
-    // {
-        // return $this->hasMany(Order::class, 'user_id');
-    // }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'user_id');
+    }
 }
