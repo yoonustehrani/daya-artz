2a5db221f55c44b32f4545700865b961df41513b
yoonus <yoonustehrani28@gmail.com>
2022-11-12 23:19:49 +0330

    enabled bot codes

--- app/Http/Controllers/Api/TelegramHookController.php
+++ app/Http/Controllers/Api/TelegramHookController.php
@@ -10,13 +10,12 @@ class TelegramHookController extends Controller
 {
     public function handle(Request $request)
     {
-        \Log::alert('hello');
-        // try {
-        //     (new DayaBot(config('services.telegram_bots.dayaartz.token')))->processRequest($request);
-        // } catch (\Throwable $th) {
-        //     \Log::error($th->getMessage());
-        //     \Log::error($th->getTraceAsString());
-        // }
+        try {
+            (new DayaBot(config('services.telegram_bots.dayaartz.token')))->processRequest($request);
+        } catch (\Throwable $th) {
+            \Log::error($th->getMessage());
+            \Log::error($th->getTraceAsString());
+        }
         return response()->json(['okay' => true]);
     }
 }
