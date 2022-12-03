b831016d8e39c0d4dbd2352c61306f5709e0b8c0
yoonus <yoonustehrani28@gmail.com>
2022-12-04 00:56:21 +0330

    telegram bot in service show page

--- resources/views/components/contact-ways.blade.php
+++ resources/views/components/contact-ways.blade.php
@@ -25,20 +25,20 @@
                     <span class="back-aqua"><i class="fal fa-robot"></i></span>
                     <h4>ربات تلگرام</h4>
                     <p class="text-secondary">ثبت سفارش تخصصی و هوشمند با ربات تلگرام:</p>
-                    <a class="btn btn-gradient ltr mt-12-p soon" href="#">بزودی</a>
+                    <a class="btn btn-gradient ltr mt-12-p" href="https://t.me/DayaArtzBot">شروع سفارش تلگرامی</a>
                 </div>
                 <div class="col-10 mb-2 mb-md-0 col-md-2 p-2 text-center">
                     <span class="back-aqua"><i class="fal fa-space-shuttle"></i></span>
                     <h4>سفارش سریع</h4>
                     <p class="text-secondary">ثبت سفارش شما به صورت فوری و سریع توسط فرم ها</p>
-                    <button class="btn btn-gradient ltr scroll-to-form">سفارش</button>
+                    <button class="btn btn-gradient ltr scroll-to-form">شروع سفارش سریع</button>
                 </div>
                 <div class="col-10 mb-2 mb-md-0 col-md-2 p-2 text-center">
                     <span class="back-aqua"><i class="fal fa-mobile"></i></span>
                     <h4>پیام رسان</h4>
                     <p class="text-secondary">از طریق واتساپ و تلگرام سفارش خود را ثبت کنید</p>
-                    <a class="btn btn-gradient ltr" href="https://telegram.me/dayasales">0901 504 5025</a>
-                    <a class="btn btn-gradient ltr" href="https://wa.me/989016045025">0901 604 5025</a>
+                    <a class="btn btn-gradient ltr" href="https://telegram.me/dayasales"><i class="fab fa-telegram-plane"></i> 0901 504 5025</a>
+                    <a class="btn btn-gradient ltr" href="https://wa.me/989016045025"><i class="fab fa-whatsapp"></i> 0901 604 5025</a>
                 </div>
             </div>
         </div>
