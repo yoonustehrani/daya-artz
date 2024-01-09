6842f73606547a367d6309a361bdc12a869790df
yoonus <yoonustehrani28@gmail.com>
2024-01-09 22:25:19 +0330

    two ads

--- resources/views/pages/posts/show.blade.php
+++ resources/views/pages/posts/show.blade.php
@@ -79,8 +79,9 @@
             </div>
         </div>
         <div class="section post-main-section p-2 mt-4">
-            <div id="pos-article-text-card-92258">
+            <div id="pos-article-display-card-92247">
                 <div class="post-main-text" id="post-content">
+                    <div id="pos-article-text-card-92258"></div>
                     {!! $post->body !!}
                 </div>
             </div>
