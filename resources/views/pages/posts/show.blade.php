de39158a91c5258ec5d83c2e25a90542100238f9
yoonus <yoonustehrani28@gmail.com>
2024-01-09 22:22:51 +0330

    new change ۲

--- resources/views/pages/posts/show.blade.php
+++ resources/views/pages/posts/show.blade.php
@@ -80,7 +80,11 @@
         </div>
         <div class="section post-main-section p-2 mt-4">
             <div id="pos-article-display-card-92247">
-                <div class="post-main-text" id="post-content">{!! $post->body !!}</div>
+                <div class="post-main-text" id="post-content">
+                    <div id="pos-article-text-card-92258">
+                        {!! $post->body !!}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
